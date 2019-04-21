9222664972c3e882b5f4ce84259dfc653a31e55d
Davide Scalzo <davidescalzo@Davides-MacBook-Pro.local>
2019-04-22 00:09:09 +0100

    user and owner tests

--- src/resources/organization/moduleOwners.test.ts
+++ src/resources/organization/moduleOwners.test.ts
@@ -1,263 +1,271 @@
 import { membersPermissionFilter } from  './permissionModule';
 
+export const generateState = () => ({
+    name: 'INITIAL NAME',
+    members: [{
+        role: 'OWNER',
+        user: {
+            id: '0',
+        },
+    }, {
+        role: 'OWNER',
+        user: {
+            id: '1',
+        },
+    },{
+        role: 'ADMIN',
+        user: {
+            id: '2',
+        },
+    },{
+        role: 'ADMIN',
+        user: {
+            id: '3',
+        },
+    },{
+        role: 'USER',
+        user: {
+            id: '4',
+        },
+    },{
+        role: 'USER',
+        user: {
+            id: '5',
+        },
+    }]
+})
 
 describe('OWNER Permissions Tests', () => {
+    it('should allow an OWNER to change name of the organization', () => {
+        const initialState = generateState()
+        const newState = generateState();
+        newState.name = 'NEW NAME'
+        const result = membersPermissionFilter(initialState, newState, '0')
+        expect(result).toBe(true)
+    })
     it('should not allow last OWNER to remove himself, if the last one', () => {
-        const initialState = {
-            members: [{
-                role: 'OWNER',
-                user: {
-                    id: '123'
-                },
-            }]
-        }
-
-        const newState = {
-            members: []
-        }
-        const result = membersPermissionFilter(initialState, newState, '123')
+        const initialState = generateState()
+            initialState.members.splice(0,1)
+        const newState = generateState();
+            newState.members.splice(0,2)
+        const result = membersPermissionFilter(initialState, newState, '1')
         expect(result).toBe(false)
     })
 
     it('should allow an OWNER to remove another OWNER', () => {
-        const initialState = {
-            members: [{
-                role: 'OWNER',
-                user: {
-                    id: '123'
-                },
-            }, {
-                role: 'OWNER',
-                user: {
-                    id: '456'
-                }
-            }]
-        }
-
-        const newState = {
-            members: [{
-                role: 'OWNER',
-                user: {
-                    id: '123'
-                },
-            }]
-        }
-        const result = membersPermissionFilter(initialState, newState, '123')
+        const initialState = generateState()
+        const newState = generateState();
+            newState.members.splice(0,1);
+        const result = membersPermissionFilter(initialState, newState, '1')
         expect(result).toBe(true);
     })
 
     it('should allow an OWNER to edit another OWNER', () => {
-        const initialState = {
-            members: [{
-                role: 'OWNER',
-                user: {
-                    id: '123'
-                },
-            }, {
-                role: 'OWNER',
-                user: {
-                    id: '456'
-                }
-            }]
-        }
-
-        const newState = {
-            members: [{
-                role: 'OWNER',
-                user: {
-                    id: '123'
-                },
-            },  {
-                role: 'ADMIN',
-                user: {
-                    id: '456'
-                }
-            }]
-        }
-        const result = membersPermissionFilter(initialState, newState, '123')
+        const initialState = generateState()
+        const newState = generateState();
+            newState.members[0].role === 'ADMIN'
+        const result = membersPermissionFilter(initialState, newState, '1')
         expect(result).toBe(true);
     })
 
     it('should allow an OWNER to remove an ADMIN', () => {
-        const initialState = {
-            members: [{
-                role: 'OWNER',
-                user: {
-                    id: '123'
-                },
-            }, {
-                role: 'ADMIN',
-                user: {
-                    id: '456'
-                }
-            }]
-        }
-
-        const newState = {
-            members: [{
-                role: 'OWNER',
-                user: {
-                    id: '123'
-                },
-            }]
-        }
-        const result = membersPermissionFilter(initialState, newState, '123')
+        const initialState = generateState()
+        const newState = generateState();
+            newState.members.splice(2,1);
+        const result = membersPermissionFilter(initialState, newState, '0')
         expect(result).toBe(true);
     })
 
     it('should allow an OWNER to edit an ADMIN', () => {
-        const initialState = {
-            members: [{
-                role: 'OWNER',
-                user: {
-                    id: '123'
-                },
-            }, {
-                role: 'ADMIN',
-                user: {
-                    id: '456'
-                }
-            }]
-        }
-
-        const newState = {
-            members: [{
-                role: 'OWNER',
-                user: {
-                    id: '123'
-                },
-            }, {
-                role: 'USER',
-                user: {
-                    id: '456'
-                }
-            }]
-        }
-        const result = membersPermissionFilter(initialState, newState, '123')
+        const initialState = generateState()
+        const newState = generateState();
+            newState.members[2].role === "USER"
+        const result = membersPermissionFilter(initialState, newState, '0')
         expect(result).toBe(true);
     })
 
     it('should allow an OWNER to remove a USER', () => {
-        const initialState = {
-            members: [{
-                role: 'OWNER',
-                user: {
-                    id: '123'
-                },
-            }, {
-                role: 'USER',
-                user: {
-                    id: '456'
-                }
-            }]
-        }
-
-        const newState = {
-            members: [{
-                role: 'OWNER',
-                user: {
-                    id: '123'
-                },
-            }]
-        }
-        const result = membersPermissionFilter(initialState, newState, '123')
+        const initialState = generateState()
+        const newState = generateState();
+            newState.members.splice(4,1);
+        const result = membersPermissionFilter(initialState, newState, '0')
         expect(result).toBe(true);
     })
 
     it('should allow an OWNER to edit a USER', () => {
-        const initialState = {
-            members: [{
-                role: 'OWNER',
-                user: {
-                    id: '123'
-                },
-            }, {
-                role: 'USER',
-                user: {
-                    id: '456'
-                }
-            }]
-        }
-
-        const newState = {
-            members: [{
-                role: 'OWNER',
-                user: {
-                    id: '123'
-                },
-            }, {
-                role: 'ADMIN',
-                user: {
-                    id: '456'
-                }
-            }]
-        }
-        const result = membersPermissionFilter(initialState, newState, '123')
+        const initialState = generateState()
+        const newState = generateState();
+            newState.members[4].role === "ADMIN"
+        const result = membersPermissionFilter(initialState, newState, '0')
         expect(result).toBe(true);
     })
 
     it('should allow an OWNER to leave the organization, if not the last one', () => {
-        const initialState = {
-            members: [{
-                role: 'OWNER',
-                user: {
-                    id: '123'
-                },
-            }, {
-                role: 'OWNER',
-                user: {
-                    id: '456'
-                }
-            }]
-        }
-
-        const newState = {
-            members: [{
-                role: 'OWNER',
-                user: {
-                    id: '456'
-                },
-            }]
-        }
-        const result = membersPermissionFilter(initialState, newState, '123')
+        const initialState = generateState()
+        const newState = generateState();
+            newState.members.splice(0,1)
+        const result = membersPermissionFilter(initialState, newState, '0')
         expect(result).toBe(true);
     })
 
     it('should allow an OWNER to edit himself, if not the last one', () => {
-        const initialState = {
-            members: [{
-                role: 'OWNER',
-                user: {
-                    id: '123'
-                },
-            }, {
-                role: 'OWNER',
-                user: {
-                    id: '456'
-                }
-            }]
-        }
+        const initialState = generateState()
+        const newState = generateState();
+            newState.members[0].role === "ADMIN"
+        const result = membersPermissionFilter(initialState, newState, '0')
+        expect(result).toBe(true);
+    })
 
-        const newState = {
-            members: [{
-                role: 'OWNER',
+    it('should allow an OWNER invite a USER', () => {
+        const initialState = generateState()
+        const newState = generateState();
+            newState.members.push({
+                role: 'USER',
                 user: {
-                    id: '123'
-                },
-            },  {
+                    id: 'new'
+                }
+            })
+        const result = membersPermissionFilter(initialState, newState, '0')
+        expect(result).toBe(true);
+    })
+
+    it('should allow an OWNER invite an ADMIN', () => {
+        const initialState = generateState()
+        const newState = generateState();
+            newState.members.push({
                 role: 'ADMIN',
                 user: {
-                    id: '456'
+                    id: 'new'
                 }
-            }]
-        }
-        const result = membersPermissionFilter(initialState, newState, '456')
+            })
+        const result = membersPermissionFilter(initialState, newState, '0')
+        expect(result).toBe(true);
+    })
+
+    it('should allow an OWNER invite an OWNER', () => {
+        const initialState = generateState()
+        const newState = generateState();
+            newState.members.push({
+                role: 'OWNER',
+                user: {
+                    id: 'new'
+                }
+            })
+        const result = membersPermissionFilter(initialState, newState, '0')
         expect(result).toBe(true);
     })
 })
 
-// CHECK FOR OWNERS TO INVITE USERS AND ADMINS
-// CHECK FOR ADMINS TO INVITE USERS
-// CHECK FOR USERS NOT ABLE TO INVITE ANYONE
-// CHECK IF USERS DOES NOT HAVE A ROLE FOR THIS ORG
-// CHECKS FOR EDITING THE NAME OF THE ORGANIZATION (ONLY OWNERS CAN)
+describe('USER Permissions Tests', () => {
+    it('should not allow a USER to change name of the organization', () => {
+        const initialState = generateState()
+        const newState = generateState();
+        newState.name = 'NEW NAME'
+        const result = membersPermissionFilter(initialState, newState, '5')
+        expect(result).toBe(false)
+    })
+    it('should not allow a USER to remove a OWNER', () => {
+        const initialState = generateState()
+        const newState = generateState();
+            newState.members.splice(0,1)
+        const result = membersPermissionFilter(initialState, newState, '5')
+        expect(result).toBe(false)
+    })
+
+    it('should not allow a USER to edit a OWNER', () => {
+        const initialState = generateState()
+        const newState = generateState();
+            newState.members[0].role === 'ADMIN'
+        const result = membersPermissionFilter(initialState, newState, '5')
+        expect(result).toBe(false);
+    })
+
+    it('should not allow a USER to remove an ADMIN', () => {
+        const initialState = generateState()
+            initialState.members.splice(0,1)
+        const newState = generateState();
+            newState.members.splice(2,1)
+        const result = membersPermissionFilter(initialState, newState, '5')
+        expect(result).toBe(false);
+    })
+
+    it('should not allow an USER to edit an ADMIN', () => {
+        const initialState = generateState()
+        const newState = generateState();
+            newState.members[2].role === 'USER'
+        const result = membersPermissionFilter(initialState, newState, '5')
+        expect(result).toBe(false);
+    })
+
+    it('should not allow a USER to remove a USER', () => {
+        const initialState = generateState()
+            initialState.members.splice(0,1)
+        const newState = generateState();
+            newState.members.splice(4,1)
+        const result = membersPermissionFilter(initialState, newState, '5')
+        expect(result).toBe(false);
+    })
+
+    it('should not allow a USER to edit a USER', () => {
+        const initialState = generateState()
+        const newState = generateState();
+            newState.members[4].role === 'ADMIN'
+        const result = membersPermissionFilter(initialState, newState, '5')
+        expect(result).toBe(false);
+    })
+
+    it('should allow a USER to leave the organization', () => {
+        const initialState = generateState()
+        const newState = generateState();
+            newState.members.splice(5,1)
+        const result = membersPermissionFilter(initialState, newState, '5')
+        expect(result).toBe(true);
+    })
+
+    it('should not allow a USER invite an OWNER', () => {
+        const initialState = generateState()
+        const newState = generateState();
+        newState.members.push({
+            role: 'OWNER',
+            user: {
+                id: 'new'
+            }
+        })
+        const result = membersPermissionFilter(initialState, newState, '5')
+        expect(result).toBe(false);
+    })
+
+    it('should not allow a USER invite an ADMIN', () => {
+        const initialState = generateState()
+        const newState = generateState();
+        newState.members.push({
+            role: 'ADMIN',
+            user: {
+                id: 'new'
+            }
+        })
+        const result = membersPermissionFilter(initialState, newState, '5')
+        expect(result).toBe(false);
+    })
+
+    it('should not allow a USER invite a USER', () => {
+        const initialState = generateState()
+        const newState = generateState();
+        newState.members.push({
+            role: 'USER',
+            user: {
+                id: 'new'
+            }
+        })
+        const result = membersPermissionFilter(initialState, newState, '5')
+        expect(result).toBe(false);
+    })
+
+    it('should not allow an USER to edit himself', () => {
+        const initialState = generateState()
+        const newState = generateState();
+            newState.members[5].role === "ADMIN"
+        const result = membersPermissionFilter(initialState, newState, '5')
+        expect(result).toBe(false);
+    })
+})
--- src/resources/organization/permissionModule.ts
+++ src/resources/organization/permissionModule.ts
@@ -9,7 +9,7 @@ interface IMember {
 
 export interface IPermissionInput {
     name?: String;
-    members: IMember[]
+    members?: IMember[]
 }
 
 const isOwner = R.propEq('role', 'OWNER')
@@ -17,13 +17,24 @@ const atLeastOneOwner = R.any(isOwner)
 
 
 export const membersPermissionFilter = (initialOrg: IPermissionInput, newOrg: IPermissionInput, userId: string) => {
-    const user = initialOrg.members.find(member => member.user.id === userId);
-    const newUser = newOrg.members.find(member => member.user.id === userId);
+    const user = initialOrg.members && initialOrg.members.find(member => member.user.id === userId);
+    const newUser = newOrg.members && newOrg.members.find(member => member.user.id === userId);
     const role = R.path(['role'], user);
     if (!role) {
         return false;
     }
-    if (role === 'OWNER') {
+    if (role === 'OWNER' && newOrg.members) {
         return atLeastOneOwner(newOrg.members)
+    }
+
+    if (role === 'USER') {
+        if (newOrg.name && newOrg.name !== initialOrg.name) {
+            return false;
+        }
+        if (newOrg.members && initialOrg.members && R.symmetricDifference(initialOrg.members, newOrg.members).length === 1 && !newUser) {
+            return true
+        } else {
+            return false
+        }
     }
 }
